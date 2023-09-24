717048f1b811495e7aee04ee62783af7fdfdac99
Alessandro Mian <alessandromian95@gmail.com>
2023-09-24 21:10:32 +0200

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alemian95/go-bookings/pkg/config"
+	"github.com/alemian95/go-bookings/pkg/handlers"
+	"github.com/alemian95/go-bookings/pkg/render"
+	"github.com/alexedwards/scs/v2"
+)
+
+const port = ":8081"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function
+func main() {
+
+	// change thos to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, cacheErr := render.CreateTemplateCache()
+	if cacheErr != nil {
+		log.Fatal(cacheErr)
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	fmt.Printf("Starting application on port %s\n", port)
+
+	srv := &http.Server{
+		Addr:    port,
+		Handler: routes(&app),
+	}
+
+	err := srv.ListenAndServe()
+	log.Fatal(err)
+}
